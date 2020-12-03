3a893849f9566e9027d7ddb671d7753b263a9af5
Luca Chiodini <luca@chiodini.org>
2020-12-03 12:08:03 +0100

    Day3: fix a couple of off-by-one

--- Day3/part1.hs
+++ Day3/part1.hs
@@ -2,6 +2,6 @@ main = do
     content <- readFile "input.txt"
     let rows = lines content
     let cols = length (head rows)
-    let indexes = [mod (x*3) cols | x <- [0..(length rows)]]
+    let indexes = [mod (x*3) cols | x <- [0..(length rows - 1)]]
     let chars = [row !! index | (row, index) <- zip rows indexes]
     print (length (filter (== '#') chars))
--- Day3/part2.hs
+++ Day3/part2.hs
@@ -1,6 +1,6 @@
 countTrees rows cols (slope_col, slope_row) = length (filter (== '#') chars) where
     used_rows = [0,slope_row..(length rows - 1)]
-    indexes = [mod (x*slope_col) cols | x <- [0..(length used_rows)]]
+    indexes = [mod (x*slope_col) cols | x <- [0..(length used_rows - 1)]]
     chars = [(rows !! row_idx) !! index | (row_idx, index) <- zip used_rows indexes]
 
 main = do
