e29cc646a1185b14b3672e635468ec223d463f13
Luca Chiodini <luca@chiodini.org>
2020-12-03 12:05:25 +0100

    Add Day3

---
+++ Day3/part1.hs
@@ -0,0 +1,7 @@
+main = do
+    content <- readFile "input.txt"
+    let rows = lines content
+    let cols = length (head rows)
+    let indexes = [mod (x*3) cols | x <- [0..(length rows)]]
+    let chars = [row !! index | (row, index) <- zip rows indexes]
+    print (length (filter (== '#') chars))
---
+++ Day3/part2.hs
@@ -0,0 +1,11 @@
+countTrees rows cols (slope_col, slope_row) = length (filter (== '#') chars) where
+    used_rows = [0,slope_row..(length rows - 1)]
+    indexes = [mod (x*slope_col) cols | x <- [0..(length used_rows)]]
+    chars = [(rows !! row_idx) !! index | (row_idx, index) <- zip used_rows indexes]
+
+main = do
+    content <- readFile "input.txt"
+    let rows = lines content
+    let cols = length (head rows)
+    let trees = map (countTrees rows cols) [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    print (product trees)
