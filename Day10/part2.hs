dd9acebc289f6ef718b314f427c6da6aaa57f127
Luca Chiodini <luca@chiodini.org>
2020-12-10 21:23:05 +0100

    I like it more without reverse

--- Day10/part2.hs
+++ Day10/part2.hs
@@ -6,8 +6,8 @@ findOrZero key map = case Map.lookup key map of
     Just v -> v
 
 countWays ns ways idx
-    | idx == (length ns) = ways Map.! 0
-    | otherwise = countWays ns newMap (idx + 1) where
+    | idx == -1 = ways Map.! 0
+    | otherwise = countWays ns newMap (idx - 1) where
         currentVal = ns !! idx
         currentWays = sum [findOrZero (currentVal + offset) ways | offset <- [1..3]]
         newMap = Map.insert currentVal currentWays ways
@@ -15,5 +15,5 @@ countWays ns ways idx
 main = do
     content <- readFile "input.txt"
     let nsWithoutMax = sort (0: map read (lines content))
-    let ns = (last nsWithoutMax + 3 : reverse nsWithoutMax)
-    print (countWays ns (Map.singleton (head ns) 1) 1)
+    let ns = nsWithoutMax ++ [last nsWithoutMax + 3]
+    print (countWays ns (Map.singleton (last ns) 1) (length ns - 2))
